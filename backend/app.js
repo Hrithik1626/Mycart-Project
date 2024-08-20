6894dcc6e57ac629773994467fbc38fbd770b524
Hrithik1626 <hrithiksaravanan18@gmail.com>
2024-08-20 18:41:36 +0530

    updated

--- backend/app.js
+++ backend/app.js
@@ -22,6 +22,14 @@ app.use('/api/v1/',order)
 app.use('/api/v1/',payment);
 
 
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+    app.get('*', (req, res) =>{
+        res.sendFile(path.resolve(__dirname, '../frontend/dist/index.html'))
+    })
+}
+
 app.use(errormiddleWare)
 
 
--- frontend/src/components/product/ProductDetail.js
+++ frontend/src/components/product/ProductDetail.js
@@ -121,7 +121,6 @@ export default function ProductDetail () {
                         dispatch(addCartItem(product._id, quantity))
                         toast('Cart Item Added!',{
                             type: 'success',
-                            position: toast.POSITION.BOTTOM_CENTER
                         })
                     }}
                      className="btn btn-primary d-inline ml-4"

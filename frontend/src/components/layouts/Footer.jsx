a93ca93b85e57058011bdd2ee658c32422c2c51d
Hrithik Saravanan <145556333+Hrithik1626@users.noreply.github.com>
2024-09-25 13:31:19 +0530

    Update

--- frontend/src/components/layouts/Footer.jsx
+++ frontend/src/components/layouts/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="py-1">
     <p className="text-center text-dark mt-1">
-      JVLcart - 2022-2023, All Rights Reserved
+      JVLcart - 2000-2024, All Rights Reserved @Hrithik Saravanan
     </p>
   </footer>
 
